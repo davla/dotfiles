ea029b9b0eedcb1ae9194e3bfaaea6b12caccff8
Davide Laezza <truzzialrogo@gmx.com>
2018-01-13 12:07:04 +0100

    Refactoring menu stuff, both the script and the stored files

--- menu.sh
+++ menu.sh
@@ -5,6 +5,33 @@
 
 #####################################################
 #
+#                   Functions
+#
+#####################################################
+
+# This function copies stored files to their designated system
+# location, ignoring those starting with an underscore.
+#
+# Arguments:
+#   - $1: Extension of the files to be copied.
+#   - $2: Parent directory where the stored files are located.
+#   - $3: Parent directory in the system location path.
+function copy-files {
+    local EXT="$1"
+    local SOURCE="$2"
+    local DEST="$3"
+
+    local SOURCE_FILES="Support/menu/$SOURCE/[^_]*.$SUFFIX"
+    local DEST_DIR="$HOME/.local/share/$DEST"
+
+    if [[ -e $SOURCE_FILES ]]; then
+        mkdir -p "$DEST_DIR"
+        cp -r $SOURCE_FILES "$DEST_DIR"
+    fi
+}
+
+#####################################################
+#
 #                       Layout
 #
 #####################################################
@@ -12,7 +39,7 @@
 MENU_DIR="$HOME/.config/menus"
 
 mkdir -p "$MENU_DIR"
-cp Support/xfce-applications.menu "$MENU_DIR"
+cp Support/menu/xfce-applications.menu "$MENU_DIR"
 echo 'Layout set'
 
 #####################################################
@@ -21,10 +48,7 @@ echo 'Layout set'
 #
 #####################################################
 
-DIRECTORIES_DIR="$HOME/.local/share/desktop-directories"
-
-mkdir -p "$DIRECTORIES_DIR"
-cp Support/directories/*.directory "$DIRECTORIES_DIR"
+copy-files 'directory' 'directories' 'desktop-directories'
 echo 'Directories set'
 
 #####################################################
@@ -33,11 +57,7 @@ echo 'Directories set'
 #
 #####################################################
 
-DESKTOPS_DIR="$HOME/.local/share/applications"
-
-mkdir -p "$DESKTOPS_DIR"
-cp Support/desktops/[^_]*.desktop "$DESKTOPS_DIR"
-chmod +x "$DESKTOPS_DIR"/*
+copy-files 'desktop' 'dekstops' 'applications'
 echo 'Desktops set'
 
 #####################################################
@@ -46,8 +66,5 @@ echo 'Desktops set'
 #
 #####################################################
 
-ICONS_DIR="$HOME/.local/share/icons"
-
-mkdir -p "$ICONS_DIR"
-cp -r Support/icons/[^_]* "$ICONS_DIR"
+copy-files '' 'icons' 'icons'
 echo 'Icons set'

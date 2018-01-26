3f5dd52cd8852d43980bd2b9c91f344aecac464b
Davide Laezza <truzzialrogo@gmx.com>
2018-01-26 12:10:07 +0000

    Added cmake and colorgrab

--- manually.sh
+++ manually.sh
@@ -19,6 +19,36 @@ fi
 
 #####################################################
 #
+#                   Colorgrab
+#
+#####################################################
+
+COLORGRAB_HOME='/opt/colorgrab'
+DESKTOPS_DIR='/usr/share/applications'
+ICONS_DIR='/usr/share/icons/hicolor'
+
+apt-get install libwxgtk3.0-dev
+git clone https://github.com/Acolarh/colorgrab "$COLORGRAB_HOME"
+cd "$COLORGRAB_HOME"
+cmake .
+make
+cd - &> /dev/null
+
+find "$COLORGRAB_HOME" -type f -executable -name 'colorgrab' \
+    -exec ln -sf '{}' /usr/local/bin/colorgrab \;
+cp "$COLORGRAB_HOME/pkg/arch/colorgrab.desktop" "$DESKTOPS_DIR"
+chmod +x "$DESKTOPS_DIRS/colorgrab.desktop"
+
+cp "$COLORGRAB_HOME/img/scalable.svg" "$ICONS_DIR/scalable/apps/colorgrab.svg"
+for IMG in $COLORGRAB_HOME/img/[0-9]*x[0-9]*.png; do
+    SIZE_DIR=$(basename "$IMG" .png | xargs -i echo "$ICONS_DIR/{}/apps")
+
+    [[ -d "$SIZE_DIR" ]] && cp "$IMG" "$SIZE_DIR/colorgrab.png"
+done
+gtk-update-icon-cache "$ICONS_DIR"
+
+#####################################################
+#
 #               CPU temp throttle
 #
 #####################################################
@@ -29,8 +59,8 @@ TEMP_THROTTLE_EXEC='/usr/local/sbin/underclock'
 wget 'https://github.com/Sepero/temp-throttle/archive/stable.zip' -O \
     "$TEMP_THROTTLE_ARCH"
 
-# Unzipping only the shellscript to stdout
-# and redirecting to /sbin/underclock
+# Unzipping only the shellscript to stdout and redirecting to /sbin/underclock
 unzip -p "$TEMP_THROTTLE_ARCH" '*.sh' > "$TEMP_THROTTLE_EXEC"
+
 chmod +x "$TEMP_THROTTLE_EXEC"
 rm "$TEMP_THROTTLE_ARCH"

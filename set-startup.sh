a9678e66755904d72c933dd6d55e50330b6af0c8
Davide Laezza <truzzialrogo@gmx.com>
2018-10-20 18:42:23 +0200

    Moving startup jobs to systemd

--- set-startup.sh
+++ set-startup.sh
@@ -4,41 +4,17 @@
 
 #####################################################
 #
-#                   Variables
-#
-#####################################################
-
-ROOT_JOBS=(
-    'underclock 55 | logger -p local0.info -t UNDERCLOCK &'
-)
-
-#####################################################
-#
 #                   Root jobs
 #
 #####################################################
 
 #########################
-# Boot root commands, append to /etc/rc.local
+# Systemd services
 #########################
 
-# Line number of "exit 0" in /etc/rc.local
-RC_LOCAL_LINE=$(grep -n -x 'exit 0' /etc/rc.local | cut -d':' -f 1)
-
-# Decreasing line number to insert before "exit 0"
-RC_LOCAL_LINE=$(( RC_LOCAL_LINE - 1 ))
-
-# Empty line before custom commands
-sudo sed -i "$RC_LOCAL_LINE i\\ " /etc/rc.local
-RC_LOCAL_LINE=$(( RC_LOCAL_LINE + 1 ))
-
-# Writing jobs to /etc/rc.local
-for COMMAND in "${ROOT_JOBS[@]}"; do
-    if ! grep "$COMMAND" /etc/rc.local &> /dev/null; then
-        sudo sed -i "$RC_LOCAL_LINE i\\$COMMAND" /etc/rc.local
-        RC_LOCAL_LINE=$(( $RC_LOCAL_LINE + 1 ))
-    fi
-done
+cp Support/startup/*.service /etc/systemd/system
+find Support/startup -name '*.service' -exec basename '{}' '.service' \; \
+    | xargs echo systemctl enable
 
 #########################
 # /etc/fstab setup
@@ -64,4 +40,4 @@ fi
 #####################################################
 
 cp Support/shell/.bash_profile "$HOME"
-cp Support/shell/startup/*.desktop "$HOME/.config/autostart"
+cp Support/startup/*.desktop "$HOME/.config/autostart"

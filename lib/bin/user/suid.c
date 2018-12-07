860912b6277e52844f73a7981996036324c67edb
Davide Laezza <truzzialrogo@gmx.com>
2018-12-07 21:21:25 +0100

    Comments and minor changes in bins

--- lib/bin/user/suid.c
+++ lib/bin/user/suid.c
@@ -9,19 +9,13 @@
 
 /*
 
-This file emulates the suid bit,
-which is ignored for some file
-types, provided that this file
-runs as root
+This file emulates the suid bit, which is ignored for some file types, provided
+that this file runs as root.
 
-It sets the uid of the process
-to that of the owner of the file
-passed as first argument, much
-like suid.
+It sets the uid of the process to that of the owner of the file passed as first
+argument, much like suid.
 
-If the file requires aruments,
-they can be written as usual
-after the filename
+If the file requires aruments, they can be written as usual after the filename.
 
 */
 
@@ -34,7 +28,6 @@ int main(int argc, char** argv) {
 
 	/* Building which command */
 	/* strlen("which ") + 1 == 7 */
-
 	if (!(which = malloc((7 + strlen(argv[1])) * sizeof(char)))) {
 		perror("Error while allocating dynamic string for which command\n");
 		return 1;
@@ -43,44 +36,30 @@ int main(int argc, char** argv) {
 	strcat(which, argv[1]);
 
 	/* Executing which command */
-
 	if (!(stdout = popen(which, "r"))) {
 		perror("Error while executing which\n");
 		return 1;
 	}
 
-	/*
-
-	If nothing is returned by which, assuming
-	argv[1] to be a valid path
-
-	*/
-
+	/* If nothing is returned by which, assuming argv[1] to be a valid path */
 	if (fscanf(stdout, "%ms", &path) == EOF) {
 		free(path);
 		path = argv[1];
 	}
 
-	/* Getting file's owner by using of stat */
-
+	/* Getting file's owner by using stat */
 	if (stat(path, &data)) {
 		perror("Error while getting file's owner\n");
 		return 1;
 	}
 
 	/* Free & close */
-
 	free(which);
 	if (path != argv[1])
 		free(path);
 	pclose(stdout);
 
-	/*
-
-	Setting process' uid to owner uid
-	of passed file and executing it
-
-	*/
+	/* Setting process' uid to owner uid of passed file and executing it */
 
 	setuid(data.st_uid);
 	execvp(argv[1], argv + 2);

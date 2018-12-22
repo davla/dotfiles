645b7d0e1a32e4ded3d25673b97c51e3092e1a0d
Davide Laezza <truzzialrogo@gmx.com>
2018-12-22 18:43:31 +0100

    Turning package -> application in manually.sh and manual-install bin

--- manually.sh
+++ manually.sh
@@ -4,15 +4,15 @@
 # applications that aren't packages in a repository. It then uses such
 # machinery immediately to install them.
 
-# Every package should define the following functions:
-#   - is-installed: Exits with 0 if the package is installed,
+# Every application should define the following functions:
+#   - is-installed: Exits with 0 if the application is installed,
 #       non-zero otherwise.
-#   - install: Installs the package.
+#   - install: Installs the application.
 #   - installed-version: Prints the installed version on STDOUT.
 #   - latest-version: Prints the latest available version on STDOUT.
 #   - is-newer <v1> <v2>: Exits with 0 if v1 equal or newer than v2, non-zero
 #       otherwise.
-#   - remove: Removes the package.
+#   - remove: Removes the application.
 #
 # Such functions should be defined inside *.inst files, located in the
 # directory functions.d under the machinery installation path.
@@ -45,7 +45,7 @@ cp "$MANUAL_LIB_DIR/"*.inst "$MANUAL_FUNCTIONS_DIR"
 
 #####################################################
 #
-#           Manual installations
+#               Manual installations
 #
 #####################################################
 

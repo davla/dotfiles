5a0f6a78ae1792fb500bd0ea6208d9b6008211f9
Davide Laezza <davide.laezza@gmx.com>
2019-05-21 00:43:24 +0200

    Debugging shell and zsh setup scripts

--- dotdrop-actions/zsh.zsh
+++ dotdrop-actions/zsh.zsh
@@ -1,10 +1,10 @@
-#!/usr/bin/env sh
+#!/usr/bin/env zsh
 
 # This script is meant as a dotdrop action to be executed after zsh dotfiles
 # installation. It initializes zsh plugins and cache.
 
 # Sourcing zsh environment variables
-. "$HOME/.zshenv"
+source "$HOME/.zshenv"
 
 # Creating zsh cache directory
 mkdir -p "$ZDOTDIR/cache"
@@ -16,11 +16,10 @@ cd "$ZDOTDIR/plugins" || exit
     > plugins-before-compinit.zsh
 < lists/plugins-after-compinit.list antibody bundle \
     > plugins-after-compinit.zsh
-cd - /dev/null || exit
 
 # Initializing cache
 thefuck -a > "$ZDOTDIR/cache/thefuck"
 fasd --init zsh-hook zsh-ccomp zsh-ccomp-install zsh-wcomp zsh-wcomp-install \
     > "$ZDOTDIR/cache/fasd"
 touch "$ZDOTDIR/cache/zygal"
-zsh zygal-static > "$ZDOTDIR/cache/zygal"
+zsh -ic zygal-static > "$ZDOTDIR/cache/zygal"
--- shell.sh
+++ shell.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env sh
 
-# This script sets up shells, in particular zsh and bash
+# This script sets up shells, namely zsh and bash
 
 #######################################
 # zsh
 #######################################
 
-# zsh-variables
-export ZSH_CACHE="$HOME/.zsh/cache"
-
 # installing zsh dotfiles and executing related scripts
 cd $DOTDROP_DIR || exit
-pipenv run bash dotdrop.sh -c "$DOTDROP_CONFIG" -p zsh
+pipenv run bash dotdrop.sh install -c "$DOTDROP_CONFIG" -p zsh || exit
 cd - > /dev/null || exit
-
-unset ZSH_CACHE

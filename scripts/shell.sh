6ac68a1d4cce4d3cd2be0cd396a334066af30885
Davide Laezza <davide.laezza@gmx.com>
2019-05-25 18:24:34 +0200

    Changing dotdrop invocation

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -2,11 +2,5 @@
 
 # This script sets up shells, namely zsh and bash
 
-#######################################
-# zsh
-#######################################
-
-# installing zsh dotfiles and executing related scripts
-cd $DOTDROP_DIR || exit
-pipenv run bash dotdrop.sh install -c "$DOTDROP_CONFIG" -p zsh || exit
-cd - > /dev/null || exit
+# Installing shells dotfiles and executing related scripts
+dotdrop both install -p shell || exit

8de942c1660efe10c4585059a70937eeecdfc3c8
Davide Laezza <davide.laezza@gmx.com>
2019-05-27 22:05:34 +0200

    Reversed conditiion for ASDF shell detection

--- dotfiles/sh/plugins/init.sh
+++ dotfiles/sh/plugins/init.sh
@@ -6,7 +6,7 @@
 # asdf
 #######################################
 
-[ "$(ps --no-headings -p "$$" -o 'comm')" != 'sh' ] || {
+[ "$(ps --no-headings -p "$$" -o 'comm')" != 'sh' ] && {
     export ASDF_PATH='/opt/asdf'
     export ASDF_CONFIG_PATH="$ASDF_PATH/etc"
     export ASDF_CONFIG_FILE="$ASDF_CONFIG_PATH/.asdfrc"

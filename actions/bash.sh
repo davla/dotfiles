8eee4558e27bb20bfacd45fb2f81d3cabb0bfff5
Davide Laezza <davide.laezza@gmx.com>
2019-05-29 13:12:11 +0200

    Fixing environment loading in shell actions

--- actions/bash.sh
+++ actions/bash.sh
@@ -1,15 +1,26 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-# This script initializes bash cache.
+# This script initializes the bash dotfiles setup.
+
+#######################################
+# Loading environment
+#######################################
+
+# The shell environment needs to be loaded manually, rather than by the shell
+# itself: indeed the dotfiles are not set up yet, as this very script is meant
+# to do so, meaning that some errors would occur when the shell sources them.
+
+. "$HOME/.dotdirs"
+. "$BDOTDIR/.bashenv"
 
 #######################################
 # Initializing $BDOTDIR
 #######################################
 
-mkdir -p "$BDOTDIR/cache"
+mkdir -p "${BDOTDIR:?}/cache"
 
 #######################################
 # Initializing cache
 #######################################
 
-fasd --init bash-hook bash-ccomp bash-ccomp-install > "$BDOTDIR/cache/fasd"
+fasd --init bash-hook bash-ccomp bash-ccomp-install > "${BDOTDIR:?}/cache/fasd"
--- actions/sh.sh
+++ actions/sh.sh
@@ -1,17 +1,28 @@
 #!/usr/bin/env sh
 
-# This script initializes the POSIX shell cache.
+# This script initializes the POSIX shell dotfies setup.
+
+#######################################
+# Loading environment
+#######################################
+
+# The shell environment needs to be loaded manually, rather than by the shell
+# itself: indeed the dotfiles are not set up yet, as this very script is meant
+# to do so, meaning that some errors would occur when the shell sources them.
+
+. "$HOME/.dotdirs"
+. "$SDOTDIR/.shenv"
 
 #######################################
 # Initializing $SDOTDIR
 #######################################
 
-mkdir -p "$SDOTDIR/cache"
-mkdir -p "$SDOTDIR/plugins/data"
+mkdir -p "${SDOTDIR:?}/cache"
+mkdir -p "${SDOTDIR:?}/plugins/data"
 
 #######################################
 # Initializing cache
 #######################################
 
-thefuck --alias > "$SDOTDIR/cache/thefuck"
-fasd --init posix-alias > "$SDOTDIR/cache/fasd"
+thefuck --alias > "${SDOTDIR:?}/cache/thefuck"
+fasd --init posix-alias > "${SDOTDIR:?}/cache/fasd"

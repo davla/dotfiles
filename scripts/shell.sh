7b7b14c5ef5a27e2631d45c435266d4f0ad8513f
Davide Laezza <davide.laezza@gmx.com>
2019-05-31 13:02:01 +0200

    Adding dependencies and default shell setup to shell.sh

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -2,5 +2,22 @@
 
 # This script sets up shells, namely zsh and bash
 
-# Installing shells dotfiles and executing related scripts
+#######################################
+# Dependencies
+#######################################
+
+sudo apt-get install bash bash-completion dash zsh
+
+#######################################
+# Dotfiles
+#######################################
+
 dotdrop both install -p shell -f
+
+#######################################
+# Default shell
+#######################################
+
+ZSH_PATH="$(which zsh)"
+chsh -s "$ZSH_PATH"
+sudo chsh -s "$ZSH_PATH"

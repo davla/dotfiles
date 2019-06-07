9577e387f3cb2314884c0e3143b6d0db3ece8377
Davide Laezza <davide.laezza@gmx.com>
2019-06-07 11:53:38 +0200

    Adding mr alias

--- dotfiles/sh/aliases.sh
+++ dotfiles/sh/aliases.sh
@@ -2,7 +2,26 @@
 
 # This script defines useful (or not) POSIX shell aliases
 
-alias chuck="curl -s http://api.icndb.com/jokes/random/ | jq -r '.value.joke'"
+#######################################
+# Core commands aliases
+#######################################
+
 alias l='ls -lAFh --color=auto'
 alias mkdir='mkdir -p'
 alias root='sudo -s'
+
+#######################################
+# Third-party commands aliases
+#######################################
+
+{%@@ if user == 'root' -@@%}
+    alias mr='mr -d /opt'
+{%@@ elif user == 'user' -@@%}
+    alias mr='mr -d $HOME'
+{%@@ endif @@%}
+
+#######################################
+# New commands
+#######################################
+
+alias chuck="curl -s http://api.icndb.com/jokes/random/ | jq -r '.value.joke'"

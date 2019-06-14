989b23ec88e877081c6c63bc1fc0fda74883f40c
Davide Laezza <davide.laezza@gmx.com>
2019-06-14 21:06:46 +0200

    Moving asdf installation to setup from dotfiles

--- scripts/asdf.sh
+++ scripts/asdf.sh
@@ -27,11 +27,27 @@ install_latest() {
 }
 
 #######################################
+# Installing dependencies
+#######################################
+
+apt-get install libreadline-dev libncurses-dev libssl-dev libffi-dev
+
+#######################################
 # Installing and sourcing asdf
 #######################################
 
-dotdrop install -p asdf
+git clone 'https://github.com/asdf-vm/asdf.git' "$ASDF_PATH"
+git -C "$ASDF_PATH" describe --abbrev=0 --tags \
+    | xargs git -C "$ASDF_PATH" checkout 2> /dev/null
+mkdir -p "$ASDF_DATA_DIR"
+mkdir -p "$ASDF_CONFIG_PATH"
 source "$ASDF_PATH/asdf.sh"
+
+#######################################
+# Installing dotfiles
+#######################################
+
+dotdrop install -p asdf
 
 #######################################
 # Installing lua

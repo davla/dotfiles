35b7d89914b3eea68eecba8f670938df23c6a5f0
Davide Laezza <davide.laezza@gmx.com>
2019-06-15 02:05:39 +0200

    Squashed 'dotfiles/' changes from fe3af72..3babc1e
    3babc1e Adding network profile to config-root.yaml
    cde5edf Deleting asdf action
    17c6ca9 Adding graphical-login profile to config-root.yaml
    2d362cc Adding startup profile to config-root.yaml
    c399045 Refactoring packages action
    84b55f4 Refactoring asdf actions
    
    git-subtree-dir: dotfiles
    git-subtree-split: 3babc1e58e8e52b1ceadf9ac113046ac06a04e74

---
+++ actions/repo-keys.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+# This script installs repository keys
+
+#######################################
+# Repositoy keys
+#######################################
+
+# 379CE192D401AB61 --> Etcher
+# C6ABDCF64DB9A0B2 --> Slack
+# 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90 --> Spotify
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys '379CE192D401AB61' \
+    'C6ABDCF64DB9A0B2' '931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90' \
+    | apt-key add -
+wget -qO - 'https://packagecloud.io/AtomEditor/atom/gpgkey' | apt-key add -
+apt-key adv --keyserver pgp.mit.edu --recv-keys 5044912E | apt-key add -
+wget -qO - 'https://download.docker.com/linux/debian/gpg' | apt-key add -
+wget -qO - 'https://dl.sinew.in/keys/enpass-linux.key' | apt-key add -
+wget -qO - 'https://cli-assets.heroku.com/apt/release.key' | apt-key add -
+wget -qO - 'https://repo.skype.com/data/SKYPE-GPG-KEY' | apt-key add -
+wget -qO - 'https://www.virtualbox.org/download/oracle_vbox_2016.asc' | \
+    apt-key add -
+wget -qO - 'https://dl.yarnpkg.com/debian/pubkey.gpg' | apt-key add -
+apt-get update -oAcquire::AllowInsecureRepositories=true
+apt-get install -oAcquire::AllowInsecureRepositories=true \
+    deb-multimedia-keyring

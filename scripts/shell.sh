baca2bd0805c8f5eba4ba9d6f088a199303e6c9d
Davide Laezza <truzzialrogo@gmx.com>
2019-06-24 14:26:40 +0200

    Adding a few shell package dependencies

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -8,7 +8,7 @@
 # Installing dependencies
 #######################################
 
-sudo apt-get install bash bash-completion dash zsh
+sudo apt-get install bash bash-completion dash exa fasd thefuck zsh
 
 #######################################
 # Installing dotfiles

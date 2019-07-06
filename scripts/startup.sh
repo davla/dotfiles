c23a6d201f6c4e7f6c785122f24c8c210c886711
Davide Laezza <truzzialrogo@gmx.com>
2019-07-06 18:36:45 +0200

    Moving grub setup to dotfiles

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-# This script sets up the system startup, including boot and both system and
-# user startup jobs
+# This script sets up the system startup, including both system and user
+# startup jobs
 
 . ./.env
 
@@ -11,22 +11,6 @@
 
 # Desktop files shell globs of user startup jobs which need to be delayed
 USER_DELAYED='*npass*.desktop'
-
-#######################################
-# Setting up boot
-#######################################
-
-# The folliwing grub custom settings are set
-#	- First OS in the list as the default choice
-#	- Zero timeout
-#	- Command line options to get brightness keys to work
-
-sudo sed -r -i 's/GRUB_DEFAULT=[0-9]+/GRUB_DEFAULT=0/g' /etc/default/grub
-sudo sed -r -i 's/GRUB_TIMEOUT=[0-9]+/GRUB_TIMEOUT=0/g' /etc/default/grub
-sudo sed -r -i 's/GRUB_CMDLINE_LINUX_DEFAULT="(.*)"/GRUB_CMDLINE_LINUX_DEFAULT="\1 acpi_backlight=vendor"/g' \
-    /etc/default/grub
-
-sudo update-grub
 
 #######################################
 # Setting up system/user startup jobs

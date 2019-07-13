a3e711b60e68e105999914cc240f56f2aaac5ea2
Davide Laezza <truzzialrogo@gmx.com>
2019-07-13 22:12:14 +0200

    Adding system environment variables

---
+++ dotfiles/etc/profile.d/env-vars.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# This file contains system environment variables, that is those not specific
+# to any user
+
+#######################################
+# asdf
+#######################################
+
+export ASDF_PATH='/opt/asdf-vm'
+export ASDF_CONFIG_PATH="$ASDF_PATH/etc"
+export ASDF_CONFIG_FILE="$ASDF_CONFIG_PATH/.asdfrc"
+export ASDF_DATA_DIR="$ASDF_PATH/data"
+export ASDF_DEFAULT_TOOL_VERSIONS_FILENAME="$ASDF_CONFIG_PATH/.tool-versions"
+
+#######################################
+# dotdrop
+#######################################
+
+export DOTFILES_HOME="$HOME/Files/Programming/sys/dotfiles"

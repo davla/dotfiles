4515c95d68bd82d0ff0de3cc169fa25209b7096f
Davide Laezza <truzzialrogo@gmx.com>
2020-04-09 00:45:27 +0200

    Adding Arch locales dotfiles

--- actions/locales.sh
+++ actions/locales.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
+# This doesn't work if this script is sourced
+. "$(dirname "$0")"/../.env
+
 # This script generating locales themselves and deals with spellcheckers
 
 #######################################
@@ -20,16 +23,26 @@ echo "$LOCALES" | while read LOCALE; do
 done
 
 # Generating locales
-dpkg-reconfigure -f noninteractive locales
+case "$DISTRO" in
+    'arch')
+        locale-gen
+        ;;
+
+    'debian')
+        dpkg-reconfigure -f noninteractive locales
+        ;;
+esac
 
 #######################################
 # Dealing with spellcheckers
 #######################################
 
 # Providing en_DK spellcheckers as aliases to en_US
-for EN_US_FILE_PATH in /usr/share/hunspell/en_US.*; do
-    EN_US_FILE_NAME="$(basename "$EN_US_FILE_PATH")"
-    EN_US_EXT="$(echo "$EN_US_FILE_NAME" | cut -d '.' -f 2-)"
+if [ -d /usr/share/hunspell/ ]; then
+    for EN_US_FILE_PATH in /usr/share/hunspell/en_US.*; do
+        EN_US_FILE_NAME="$(basename "$EN_US_FILE_PATH")"
+        EN_US_EXT="$(echo "$EN_US_FILE_NAME" | cut -d '.' -f 2-)"
 
-    ln -sf "./$EN_US_FILE_NAME" "/usr/share/hunspell/en_DK.$EN_US_EXT"
-done
+        ln -sf "./$EN_US_FILE_NAME" "/usr/share/hunspell/en_DK.$EN_US_EXT"
+    done
+fi

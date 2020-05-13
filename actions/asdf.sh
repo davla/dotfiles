fdf1e0c5913d30a48ff64f801c6f2dec93da914b
Davide Laezza <truzzialrogo@gmx.com>
2020-05-13 21:10:57 +0200

    Moving asdf environment to its own dotfiles (this maneuver cost us 51 years)

--- actions/asdf.sh
+++ actions/asdf.sh
@@ -9,17 +9,18 @@
 # Input processing
 #######################################
 
-ASDF_CONFIG="$1"
+ASDF_CONFIG="$(readlink -f "$1")"
 
 #######################################
 # Linking plugin dotfiles
 #######################################
 
-# Linking every file but ".asdfrc" and ".tool-versions", which are not asdf
+# Linking every file but "asdfrc" and "tool-versions", which are not asdf
 # plugins configuration files.
 #
 # Fortunately, all plugins need their dotfiles straight in the home directory,
-# so this can all be dealt with by a single command
-find "$ASDF_CONFIG" -mindepth 1 -type f -not -name '.asdfrc' \
-        -not -name '.tool-verions' -print0 \
-    | xargs --null -i ln -sf '{}' "$HOME"
+# so this can all be dealt with by a single command. Linking also adds a
+# leading dot to the filename, as this is what the plugins require.
+find "$ASDF_CONFIG" -mindepth 1 -type f -not -name 'asdfrc' \
+        -not -name 'tool-versions' -printf '%f\0' \
+    | xargs --null -i ln -sf "$ASDF_CONFIG/{}" "$HOME/.{}"
--- python/functions.py
+++ python/functions.py
@@ -6,7 +6,41 @@ This module defines some custom functions to be used in dotdrop jinja2
 templates.
 """
 
+from jinja2 import contextfunction
+from jinja2.runtime import Context
+
 from python.lib import expand_xdg as xdg
+
+
+@contextfunction
+def vars_with_prefix(context: Context, prefix: str) -> dict:
+    """Return the variables starting with a prefix as a dict.
+
+    This function groups the available variables whose names start with a given
+    prefix into a dictionary. The keys are the variable names, with the prefix
+    stripped away. The values are the variables' values. The prefix is appended
+    an underscore, if it doesn't end with one, both when filtering the
+    variables and when stripping the dictionary keys.
+
+    :param context: Jinja context.
+    :param prefix: The prefix to filter variables. An underscore is appended if
+                   it is not the last character.
+    :return: A dictionary containing the variables whose names start with
+             prefix.
+    """
+    prefix = prefix if prefix.endswith('_') else prefix + '_'
+
+    # Injected variables are defined in context.parent, while variables created
+    # in the template are in context.vars. Thus, the two dictionaries are
+    # merged to have all the variables together. context.var is afterwards so
+    # that local variables override the injected ones.
+    vars = {**context.parent, **context.vars}
+
+    return {
+        name[len(prefix):]: value
+        for name, value in vars.items()
+        if name.startswith(prefix)
+    }
 
 
 def xdg_config(path: str) -> str:

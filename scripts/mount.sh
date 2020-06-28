acaef9b4484e21bc562e79907fdc65f846f2bf8d
Davide Laezza <truzzialrogo@gmx.com>
2020-06-28 16:13:52 +0200

    Updating work bootstrapping script

--- scripts/mount.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-mkdir -p $HOME/mounts/win-repos
-sudo /usr/bin/vmhgfs-fuse .host:/win-repos $HOME/mounts/win-repos -o subtype=vmhgfs-fuse,allow_other
-
-gpg --import $HOME/mounts/win-repos/gpg-pvt.asc
-git secret reveal

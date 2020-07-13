9ea0977d6f86d98c8318657dc6c30b560797427e
Davide Laezza <truzzialrogo@gmx.com>
2020-07-13 10:58:28 +0200

    Adding some logging to shell setup bot script

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -2,12 +2,14 @@
 
 # This script sets up shells, namely zsh and bash
 
-. ./.env
+# This doesn't work if this script is sourced
+. "$(dirname "$0")/../.env"
 
 #######################################
 # Installing dependencies
 #######################################
 
+echo '\e[32m[INFO]\e[0m Installing system dependencies'
 case "$DISTRO" in
     'arch')
         yay -S antibody-bin bash bash-completion dash exa fasd thefuck zsh
@@ -24,8 +26,11 @@ esac
 # Installing dotfiles
 #######################################
 
+echo '\e[32m[INFO]\e[0m Installing POSIX shell configuration'
 dotdrop -U both install -p sh
+echo '\e[32m[INFO]\e[0m Installing bash configuration'
 dotdrop -U both install -p bash
+echo '\e[32m[INFO]\e[0m Installing zsh configuration'
 dotdrop -U both install -p zsh
 
 #######################################
@@ -33,5 +38,7 @@ dotdrop -U both install -p zsh
 #######################################
 
 ZSH_PATH="$(which zsh)"
+echo "\e[32m[INFO]\e[0m Setting default shell to zsh for $USER"
 chsh -s "$ZSH_PATH"
+echo '\e[32m[INFO]\e[0m Setting default shell to zsh for root'
 sudo chsh -s "$ZSH_PATH"

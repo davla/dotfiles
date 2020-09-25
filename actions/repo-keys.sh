ba9512a91c5b3b0558809e0391b76e03e080439c
Davide Laezza <truzzialrogo@gmx.com>
2020-09-25 20:56:18 +0200

    Configuring Microsoft Teams

--- actions/repo-keys.sh
+++ actions/repo-keys.sh
@@ -101,6 +101,14 @@ find /etc/apt/sources.list.d/ -type f -name '*.list' -print0 \
                     > /dev/null
                 ;;
 
+            'teams')
+                echo "Installing apt repository key for $REPO"
+
+                apt-key adv --fetch-keys \
+                    'https://packages.microsoft.com/keys/microsoft.asc' \
+                    > /dev/null
+                ;;
+
             'virtualbox')
                 echo "Installing apt repository key for $REPO"
 

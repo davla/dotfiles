903593b445a7819ba558c5b7278aebc2d77b9869
Davide Laezza <truzzialrogo@gmx.com>
2020-10-24 16:48:05 +0200

    Adding utility function to load local adsf binaries into PATH

--- dotfiles/config/sh/functions.sh
+++ dotfiles/config/sh/functions.sh
@@ -42,3 +42,28 @@ t() {
 
     unset T_LEVEL
 }
+
+########################################
+# Utility functions
+########################################
+
+# This function adds to PATH the local asdf-managed executables specified for
+# the project in the current working directory
+local_asdf_in_path() {
+    echo 'Looking for local asdf executables'
+
+    # Skipping python because virtualenvs make this redundant (and not working)
+    LOCAL_ASDF_FOUND="$(asdf current | grep -vE '(system|python)' \
+        | cut -d ' ' -f 1)"
+    [ -z "$LOCAL_ASDF_FOUND" ] && {
+        echo 'No asdf local plugins found'
+        return
+    }
+    echo "Found these local plugins: $LOCAL_ASDF_FOUND" | paste -s -d ' '
+
+    echo 'Exporting local plugins in PATH'
+    export PATH="$(echo "$LOCAL_ASDF_FOUND" | xargs -n 1 asdf which \
+        | xargs -n 1 dirname | paste -s -d ':'):$PATH"
+
+    unset LOCAL_ASDF_EXCLUDE LOCAL_ASDF_FOUND
+}

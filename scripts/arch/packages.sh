9f7ba65a04686551e1f75056eb29d7b65a2cf217
Davide Laezza <truzzialrogo@gmx.com>
2021-01-24 15:39:34 +0000

    Adding more packges to Arch installation

--- scripts/arch/packages.sh
+++ scripts/arch/packages.sh
@@ -37,15 +37,49 @@ cd - > /dev/null 2>&1 || exit
 sudo -u "$USER" rm -rf "$YAY_DIR"
 
 #######################################
+# Installing GUI applications
+#######################################
+
+case "$HOST" in
+    'personal')
+        sudo -u "$USER" yay -S asunder atril baobab blueman brasero calibre \
+            code-git dropbox electron-ozone firefox-beta-bin geany gimp \
+            gnome-clocks gnome-keyring gparted gufw handbrake \
+            libreoffice-still kid3 remmina seahorse simple-scan \
+            soundconverter spotify telegram-desktop thunderbird \
+            transmission-gtk vlc
+            # balena-etcher-electron bitwarden
+        ;;
+esac
+
+# Dotfiles
+sudo -u "$USER" dotdrop install -p gui
+
+#######################################
 # Installing CLI applications
 #######################################
 
-sudo -u "$USER" yay -S antibody-bin apng2gif asdf-vm at autoconf automake \
-    certbot cmake cowsay curl ddclient dex dkms dos2unix fortune-mod gcc gdb \
-    gifsicle git-secret git-review jq lua man mercurial moreutils nfs-utils \
-    nyancat otf-ipafont p7zip pkgfile python python-pip python-pipenv sudo \
-    ttf-baekmuk ttf-dejavu ttf-indic-otf ttf-khmer unzip vim \
+case "$HOST" in
+    'personal')
+        sudo -u "$USER" yay -S cups cups-pdf dex docker docker-compose \
+            docker-credential-secretservice gdb intel-ucode libsecret \
+            hunspell hunspell-da hunspell-en_US hunspell-it pulseaudio \
+            pulseaudio-alsa pulseaudio-bluetooth pulseaudio-jack
+        ;;
+
+    'raspberry')
+        sudo -u "$USER" yay -S at certbot ddclient
+        ;;
+esac
+
+sudo -u "$USER" yay -S antibody-bin apng2gif asdf-vm autoconf automake cmake \
+    cowsay curl dkms dos2unix fortune-mod gcc ghc gifsicle git-secret \
+    git-review gnupg jq lua man mercurial moreutils multi-git-status myrepos \
+    nfs-utils nyancat otf-ipafont p7zip pkgfile python python-pip \
+    python-pipenv sudo ttf-baekmuk ttf-indic-otf ttf-khmer unzip vim \
     wqy-microhei-lite zip
+    # luacheck shellcheck rar unrar
+    # wmctrl xdotool for wayland
 
 # Dotfiles
 sudo -u "$USER" dotdrop install -p cli -U both
@@ -60,4 +94,16 @@ sudo -u "$USER" yay -Su
 # Initial setup
 #######################################
 
+# Cups (printer service)
+case "$HOST" in
+    'personal')
+        systemctl enable --now cups.socket
+        ;;
+esac
+
+# Daily pkgfile update
 systemctl enable --now pkgfile-update.timer
+
+# Docker non root access.
+groupadd -f docker
+usermod -aG docker "$USER"

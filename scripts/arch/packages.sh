842b12d8295c2791f17131944d77011f936a043a
Davide Laezza <truzzialrogo@gmx.com>
2021-01-24 22:29:49 +0000

    Configuring fonts

--- scripts/arch/packages.sh
+++ scripts/arch/packages.sh
@@ -43,7 +43,9 @@ sudo -u "$USER" rm -rf "$YAY_DIR"
 sudo -u "$USER" yay -S antibody-bin apng2gif asdf-vm at autoconf automake \
     certbot cmake cowsay curl ddclient dex dkms dos2unix fortune-mod gcc gdb \
     gifsicle git-secret git-review jq lua man mercurial moreutils nfs-utils \
-    nyancat p7zip pkgfile python python-pip python-pipenv sudo unzip vim zip
+    nyancat otf-ipafont p7zip pkgfile python python-pip python-pipenv sudo \
+    ttf-baekmuk ttf-dejavu ttf-indic-otf ttf-khmer unzip vim \
+    wqy-microhei-lite zip
 
 # Dotfiles
 sudo -u "$USER" dotdrop install -p cli -U both

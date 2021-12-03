4edf69e1e93b7944d1b1e5735fc43864bd6ccc58
Davide Laezza <truzzialrogo@gmx.com>
2021-12-03 23:03:45 +0100

    feature(packages): add libnotify-bin to debian

--- scripts/debian/packages.sh
+++ scripts/debian/packages.sh
@@ -191,8 +191,8 @@ apt-get install apt-transport-https autoconf automake build-essential cmake \
     command-not-found cowsay curl dbus-x11 dex dkms docker-ce dos2unix \
     fonts-freefont-otf fonts-nanum fortune g++ gdb git git-secret \
     gvfs-backends hunspell hunspell-en-us hunspell-it intel-microcode jq \
-    libsecret-1-dev lua5.1 lua-check make moreutils nfs-common nyancat p7zip \
-    pycodestyle python3 python3-pip rar sct shellcheck \
+    libnotify-bin libsecret-1-dev lua5.1 lua-check make moreutils nfs-common \
+    nyancat p7zip pycodestyle python3 python3-pip rar sct shellcheck \
     software-properties-common systemd-cron thunar-archive-plugin uni2ascii \
     unrar vim wmctrl xdotool xserver-xorg-input-synaptics yad zip
 

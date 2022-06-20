d2a6764e266c187e8c6ae8d37a404e70987a63e5
Davide Laezza <truzzialrogo@gmx.com>
2022-06-20 18:45:55 +0200

    feature(scripts): add shared logging facility

---
+++ scripts/lib.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+
+# This file contains utility code shared across bot scripts
+
+# This function prints a line with an informative message, that is a string
+# prefixed by a green "[INFO]"
+#
+# Arguments:
+#   - n: don't print final newline.
+#   - $1: the message to be printed.
+print_info() {
+    NEWLINE='\n'
+    while getopts ':n' OPTION; do
+        case "$OPTION" in
+            'n')
+                NEWLINE=''
+                ;;
+
+            *)
+                echo >&2 "print_info: unknown flag: $OPTION"
+                exit 63
+                ;;
+        esac
+    done
+    shift $(( OPTIND - 1 ))
+
+    printf "\e[32m[INFO]\e[0m %s$NEWLINE" "$1"
+    unset NEWLINE OPTION
+}

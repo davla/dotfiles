44e0023cdccad409d9163c63940554b41d5830df
Davide Laezza <truzzialrogo@gmx.com>
2022-11-07 23:07:59 +0100

    fix(asdf): link global default tool versions in home directories

--- dotfiles/actions/asdf.sh
+++ dotfiles/actions/asdf.sh
@@ -18,13 +18,20 @@ ASDF_CONFIG="$(readlink -f "$1")"
 # Link plugin dotfiles
 #######################################
 
-# Link every file but "asdfrc" and "tool-versions", which are not asdf
-# configuration files for plugins, but rather for asdf itself.
+# Link every file but "asdfrc", which is not an asdf configuration files for
+# plugins, but rather for asdf itself.
 #
 # Fortunately, all plugins need their dotfiles straight in the home directory,
 # so this can all be dealt with by a single command. Linking also adds a
 # leading dot to the filename, as this is what the plugins require.
 print_info 'Link asdf plugin configuration files'
-find "$ASDF_CONFIG" -mindepth 1 -type f -not -name 'asdfrc' \
-        -not -name 'tool-versions' -printf '%f\0' \
+find "$ASDF_CONFIG" -mindepth 1 -type f -not -name 'asdfrc' -printf '%f\0' \
     | xargs --null -i ln -sf "$ASDF_CONFIG/{}" "$HOME/.{}"
+
+########################################
+# Set system tool versions for all users
+########################################
+
+print_info 'Set system tool versions for all users'
+find '/home' -mindepth 1 -maxdepth 1 -type d \
+    -exec ln -sf "$ASDF_CONFIG/tool-versions" '{}/.tool-versions' \;

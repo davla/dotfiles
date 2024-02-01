ecacc07b49aa2cfb8b1d0b9913fb8cdced1a819c
Davide Laezza <truzzialrogo@gmx.com>
2024-02-01 14:54:15 +0100

    feature(work): remove unnecessary configuration
    I'm running linux natively!

--- dotfiles/actions/repo-keys.sh
+++ dotfiles/actions/repo-keys.sh
@@ -70,10 +70,6 @@ find /etc/apt/sources.list.d/ -type f -name '*.sources' -print0 \
     | xargs -0 -I '{}' basename '{}' '.sources' | tr '[:upper:]' '[:lower:]' \
     | while read -r REPO; do
         case "$REPO" in
-            'alacritty')
-                download_key_from_keyserver "$REPO" 'D43B5377'
-                ;;
-
             'deb-multimedia')
                 print_info "Installing apt repository key for $REPO"
 
@@ -98,16 +94,12 @@ find /etc/apt/sources.list.d/ -type f -name '*.sources' -print0 \
                     'https://packages.mozilla.org/apt/repo-signing-key.gpg'
                 ;;
 
-            'mono')
-                download_key_from_keyserver "$REPO" 'D3D831EF'
-                ;;
-
             'spotify')
                 download_key_from_url "$REPO" \
                     'https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg'
                 ;;
 
-            'azure-cli'|'microsoft-prod'|'teams'|'vscode')
+            'microsoft-prod'|'vscode'|'teams')
                 download_key_from_url "$REPO" \
                     'https://packages.microsoft.com/keys/microsoft.asc'
                 ;;

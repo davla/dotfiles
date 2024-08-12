69986fcc53d81e905cb08cf3c7e2b89e55427776
Davide Laezza <truzzialrogo@gmx.com>
2024-08-12 22:32:55 +0200

    refactor(shells): add environment variable based configurations

---
+++ dotfiles/dotfiles/config/sh/interactive/env-var-config.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env sh
+
+# This file contains environment variables used to configure some commands when
+# used interactively.
+
+#######################################
+# exa / ls
+#######################################
+
+EXA_COLORS="$(grep --invert-match --perl-regexp '(^#|^\s*$)' \
+        "$SDOTDIR/interactive/dotfiles/exa_colors" \
+    | paste --serial --delimiters ':')"
+export EXA_COLORS LS_COLORS="$EXA_COLORS"
+
+########################################
+# grep
+########################################
+
+GREP_COLORS="$(grep --invert-match --perl-regexp '(^#|^\s*$)' \
+        "$SDOTDIR/interactive/dotfiles/grep_colors" \
+    | paste --serial --delimiters ':')"
+export GREP_COLORS
+
+########################################
+# jq
+########################################
+
+JQ_COLORS="$(grep --invert-match --perl-regexp '(^#|^\s*$)' \
+        "$SDOTDIR/interactive/dotfiles/jq_colors" \
+    | paste --serial --delimiters ':')"
+export JQ_COLORS
+
+########################################
+# less
+########################################
+
+export LESS='--chop-long-lines --RAW-CONTROL-CHARS'
+
+########################################
+# unzip
+########################################
+
+export UNZIP='-a -L -o'
+
+########################################
+# Widely used environment variables
+########################################
+
+export EDITOR='vim'
+export PAGER="less --chop-long-lines --no-init --quit-if-one-screen \
+    --RAW-CONTROL-CHARS"
+export VISUAL="$EDITOR"

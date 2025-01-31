ae4b147ee44e48f6139de71aff53b540998a61c6
Davide Laezza <truzzialrogo@gmx.com>
2025-01-31 19:28:55 +0100

    refactor(asdf): update utilities

--- dotfiles/dotfiles/config/sh/functions.sh
+++ dotfiles/dotfiles/config/sh/functions.sh
@@ -1,30 +1,4 @@
 #!/usr/bin/env sh
 
-# This scripts defines some POSIX shell functions, used in bot interactive and
-# non-interactive shells
-
-########################################
-# Utility functions
-########################################
-
-# This function adds to PATH the local asdf-managed executables specified for
-# the project in the current working directory
-local_asdf_in_path() {
-    echo 'Looking for local asdf executables'
-
-    # Skipping python because virtualenvs make this redundant (and not working)
-    LOCAL_ASDF_FOUND="$(asdf current | grep -vE '(system|python)' \
-        | cut -d ' ' -f 1)"
-    [ -z "$LOCAL_ASDF_FOUND" ] && {
-        echo 'No asdf local plugins found'
-        return
-    }
-    echo "Found these local plugins: $LOCAL_ASDF_FOUND" | paste -s -d ' '
-
-    echo 'Exporting local plugins in PATH'
-    LOCAL_ASDF_PATH="$(echo "$LOCAL_ASDF_FOUND" | xargs -n 1 asdf which \
-        | xargs -n 1 dirname | paste -s -d ':')"
-    export PATH="$LOCAL_ASDF_PATH:$PATH"
-
-    unset LOCAL_ASDF_EXCLUDE LOCAL_ASDF_FOUND LOCAL_ASDF_PATH
-}
+# This scripts defines some POSIX shell functions, used in both interactive and
+# non-interactive shells. At present, none. ^_^"

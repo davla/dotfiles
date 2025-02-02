e3e45a42e74acf9bdcfe9383c0f74c2783836e7d
Davide Laezza <truzzialrogo@gmx.com>
2025-02-02 23:53:02 +0100

    refactor(dotdrop): use uv instead of pipenv

--- scripts/dotdrop.sh
+++ scripts/dotdrop.sh
@@ -15,28 +15,33 @@ DOTDROP_DIR="$1"
 # Create dotdrop virtualenv
 #######################################
 
-print_info 'Install pipenv'
+print_info 'Install uv'
 case "$DISTRO" in
     'arch')
-        sudo pacman -S --needed gcc python python-pipenv
+        sudo pacman -S --needed uv
         ;;
 
     'debian')
-        sudo apt-get update
-        sudo apt-get install gcc python3 pipenv
+        which uv > /dev/null 2>&1 ||
+            # Quick-and-dirty uv installation. github-releases will take over
+            # once provisioning is complete
+            uname --machine | xargs -I '{}' wget --quiet --output-document - \
+                    'https://github.com/astral-sh/uv/releases/latest/download/uv-{}-unknown-linux-gnu.tar.gz' \
+                | sudo tar --extract --gzip --strip-components 1 \
+                    --directory /usr/local/bin
         ;;
 esac
 
 cd "$DOTDROP_DIR" || exit
 
-print_info 'Clear existing virtual environment'
-sudo rm -rf .venv
+print_info 'Recreate empty virtual environment'
+if { which asdf && asdf which python; } > /dev/null 2>&1; then
+    asdf which python | xargs uv venv --python
+else
+    uv venv
+fi
 
 print_info 'Install project dependencies'
-if { which asdf && asdf which python; } > /dev/null 2>&1; then
-    asdf which python | xargs pipenv install --dev --python
-else
-    pipenv install --dev
-fi
+uv sync
 
 cd - > /dev/null 2>&1 || exit

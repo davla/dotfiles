ba0c0234ffccdc6a8c7c8f3db6168a73b88d6899
Davide Laezza <truzzialrogo@gmx.com>
2025-03-14 23:28:22 +0100

    feature(zsh): use sheldon to load theme

--- dotfiles/dotfiles/config/zsh/interactive/theme/init.zsh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-
-# This scripts loads the zsh theme
-
-#######################################
-# Sourcing zygal static script
-#######################################
-
-source "$ZCACHEDIR/zygal"

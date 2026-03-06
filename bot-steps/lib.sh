96bb7f076792edf8456afa3c8d5f69e087f9caa3
Davide Laezza <truzzialrogo@gmx.com>
2026-03-06 20:45:21 +0100

    refactor(bot): account for unauthenticated gh-release

--- bot-steps/lib.sh
+++ bot-steps/lib.sh
@@ -27,3 +27,23 @@ print_info() {
     printf "\e[32m[INFO]\e[0m %s$NEWLINE" "$1"
     unset NEWLINE OPTION
 }
+
+# This function installs a package via gh-release but only if the GitHub CLI is
+# already authenticated.
+#
+# Arguments:
+#   - $1: The package to be installed via gh-releases
+gh_release_install() {
+    GH_RELEASE_INSTALL__PACKAGE="$1"
+
+    gh auth status --active > /dev/null 2>&1 || {
+        print_info -n "GitHub CLI not authenticated. Can't install "
+        echo "$GH_RELEASE_INSTALL__PACKAGE via gh-releases"
+        exit 0
+    }
+
+    print_info "Install $GH_RELEASE_INSTALL__PACKAGE via gh-release"
+    sudo gh-release install "$GH_RELEASE_INSTALL__PACKAGE"
+
+    unset GH_RELEASE_INSTALL__PACKAGE
+}

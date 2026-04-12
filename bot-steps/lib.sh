d1c33a6187215ca062472338ab3b37c1e70a332f
Davide Laezza <truzzialrogo@gmx.com>
2026-04-13 00:03:25 +0200

    refactor: use subshell for shell functions to implement local variables

--- bot-steps/lib.sh
+++ bot-steps/lib.sh
@@ -2,13 +2,27 @@
 
 # This file contains utility code shared across bot steps
 
+# This function returns the absolute paths where the files belonging to a
+# dotdrop profile are installed to.
+#
+# Arguments:
+#   - $1: The dotdrop profile whose files will be returned
+#   - $2: The user the dotdrop profile belongs to. Optional, defaults to "user"
+dotdrop_files() (
+    DOTDROP_PROFILE="$1"
+    DOTDROP_USER="${2:-user}"
+
+    dotdrop files -bGp "$DOTDROP_PROFILE" -U "$DOTDROP_USER" 2> /dev/null \
+        | cut --delimiter ',' --fields 2 | cut --delimiter ':' --fields 2
+)
+
 # This function prints a line with an informative message, that is a string
 # prefixed by a green "[INFO]"
 #
 # Arguments:
 #   - n: don't print final newline.
 #   - $1: the message to be printed.
-print_info() {
+print_info() (
     NEWLINE='\n'
     while getopts ':n' OPTION; do
         case "$OPTION" in
@@ -25,5 +39,4 @@ print_info() {
     shift $(( OPTIND - 1 ))
 
     printf "\e[32m[INFO]\e[0m %s$NEWLINE" "$1"
-    unset NEWLINE OPTION
-}
+)
--- dotfiles/dotfiles/usr/local/lib/sh/gh-release.sh
+++ dotfiles/dotfiles/usr/local/lib/sh/gh-release.sh
@@ -28,10 +28,10 @@ ZSH_COMPLETIONS_DIR='/usr/local/share/zsh/site-functions'
 # Arguments:
 #   - $1: One of 'x86_64' or 'amd64'. Controls what's output for the x86-64
 #         architechture. Optional, defaults to 'x86_64'
-cpu_arch() {
-    CPU_ARCH__FORMAT="${1:-x86_64}"
+cpu_arch() (
+    FORMAT="${1:-x86_64}"
 
-    case "$(echo "$CPU_ARCH__FORMAT" | tr '[:upper:]' '[:lower:]')" in
+    case "$(echo "$FORMAT" | tr '[:upper:]' '[:lower:]')" in
         'x86_64')
             uname --machine
             ;;
@@ -41,12 +41,10 @@ cpu_arch() {
             ;;
 
         *)
-            echo >&2 "Unknown output format: $CPU_ARCH__FORMAT"
+            echo >&2 "Unknown output format: $FORMAT"
             ;;
     esac
-
-    unset CPU_ARCH__FORMAT
-}
+)
 
 # This function removes its arguments. It is mean to be used as a script
 # termination trap.
@@ -72,45 +70,39 @@ cleanup_trap() {
 #         Optional, defults to extracting all the files in the archive
 #   - $5: The directory where the tar archive is extracted to. Optional,
 #         defaults to `/usr/local/bin`
-install_github_release_tar() {
-    GH_RELEASE_TAR__REPO="$1"
-    GH_RELEASE_TAR__TAG="$2"
-    GH_RELEASE_TAR__ARCHIVE_GLOB="$3"
-    GH_RELEASE_TAR__BIN_GLOB="${4:-*}"
-    GH_RELEASE_TAR__DST_DIR="${5:-/usr/local/bin}"
+install_github_release_tar() (
+    GITHUB_REPO="$1"
+    RELEASE_TAG="$2"
+    ARCHIVE_GLOB="$3"
+    BIN_GLOB="${4:-*}"
+    DST_DIR="${5:-/usr/local/bin}"
 
-    GH_RELEASE_TAR__COMPRESS=''
-    case "$GH_RELEASE_TAR__ARCHIVE_GLOB" in
+    COMPRESS=''
+    case "$ARCHIVE_GLOB" in
         *.tar.gz)
-            GH_RELEASE_TAR__COMPRESS='--gzip'
+            COMPRESS='--gzip'
             ;;
 
         *.tar.bz2)
-            GH_RELEASE_TAR__COMPRESS='--bzip2'
+            COMPRESS='--bzip2'
             ;;
 
         *.tar.xz)
-            GH_RELEASE_TAR__COMPRESS='--xz'
+            COMPRESS='--xz'
             ;;
     esac
 
-    GH_RELEASE_TAR__PACKAGE="${GH_RELEASE_TAR__REPO##*/}"
-    printf 'Download and install %s to %s...\n' "$GH_RELEASE_TAR__PACKAGE" \
-        "$GH_RELEASE_TAR__DST_DIR" | log_debug
-    printf 'Use %s to extract %s\n' "$GH_RELEASE_TAR__COMPRESS" \
-        "$GH_RELEASE_TAR__PACKAGE" | log_debug
+    PACKAGE_NAME="${GITHUB_REPO##*/}"
+    printf 'Download and install %s to %s...\n' "$PACKAGE_NAME" "$DST_DIR" \
+        | log_debug
+    printf 'Use %s to extract %s\n' "$COMPRESS" "$PACKAGE_NAME" | log_debug
 
     # The --transform option to tar extracts the basename
-    gh --repo "$GH_RELEASE_TAR__REPO" release download "$GH_RELEASE_TAR__TAG" \
-            --output - --pattern "$GH_RELEASE_TAR__ARCHIVE_GLOB" \
-        | tar --extract "$GH_RELEASE_TAR__COMPRESS" --transform 's|.*/||g' \
-            --directory "$GH_RELEASE_TAR__DST_DIR" \
-            --wildcards "$GH_RELEASE_TAR__BIN_GLOB"
-
-    unset GH_RELEASE_TAR__ARCHIVE_GLOB GH_RELEASE_TAR__BIN_GLOB \
-        GH_RELEASE_TAR__COMPRESS GH_RELEASE_TAR__DST_DIR \
-        GH_RELEASE_TAR__REPO GH_RELEASE_TAR__TAG
-}
+    gh --repo "$GITHUB_REPO" release download "$RELEASE_TAG" --output - \
+            --pattern "$ARCHIVE_GLOB" \
+        | tar --extract "$COMPRESS" --transform 's|.*/||g' \
+            --directory "$DST_DIR" --wildcards "$BIN_GLOB"
+)
 
 # This function downloads and installs a .deb file from the assets of a GitHub
 # release.
@@ -119,27 +111,23 @@ install_github_release_tar() {
 #   - $1: The GitHub repository that made the release
 #   - $2: The GitHub release tag
 #   - $3: A glob matching the .deb release artifact by name
-install_github_release_deb() {
-    GH_RELEASE_DEB__REPO="$1"
-    GH_RELEASE_DEB__TAG="$2"
-    GH_RELEASE_DEB__GLOB="$3"
+install_github_release_deb() (
+    GITHUB_REPO="$1"
+    RELEASE_TAG="$2"
+    DEB_GLOB="$3"
 
-    GH_RELEASE_DEB__PACKAGE="${GH_RELEASE_DEB__REPO##*/}"
-    GH_RELEASE_DEB__TMP_DEB="$(mktemp --dry-run \
-        "XXX-$GH_RELEASE_DEB__PACKAGE.deb")"
+    PACKAGE_NAME="${GITHUB_REPO##*/}"
+    TMP_DEB="$(mktemp --dry-run "XXX-$PACKAGE_NAME.deb")"
     # shellcheck disable=SC2064
-    trap "cleanup_trap $GH_RELEASE_DEB__TMP_DEB" EXIT INT HUP TERM
+    trap "cleanup_trap $TMP_DEB" EXIT INT HUP TERM
 
-    log_debug "Download $GH_RELEASE_DEB__PACKAGE..."
-    gh --repo "$GH_RELEASE_DEB__REPO" release download "$GH_RELEASE_DEB__TAG" \
-        --output "$GH_RELEASE_DEB__TMP_DEB" --pattern "$GH_RELEASE_DEB__GLOB"
+    log_debug "Download $PACKAGE_NAME..."
+    gh --repo "$GITHUB_REPO" release download "$RELEASE_TAG" \
+        --output "$TMP_DEB" --pattern "$DEB_GLOB"
 
-    log_debug "Install $GH_RELEASE_DEB__PACKAGE..."
-    dpkg --install "$GH_RELEASE_DEB__TMP_DEB" | log_debug
-
-    unset GH_RELEASE_DEB__GLOB GH_RELEASE_DEB__PACKAGE GH_RELEASE_DEB__REPO \
-        GH_RELEASE_DEB__TAG GH_RELEASE_DEB__TMP_DEB
-}
+    log_debug "Install $PACKAGE_NAME..."
+    dpkg --install "$TMP_DEB" | log_debug
+)
 
 # This function installs a bash completion file received via STDIN.
 #
@@ -147,17 +135,15 @@ install_github_release_deb() {
 #   - STDIN: The content of the bash completion.
 #   - $1: The name for the completion file. If it's a path, only the base name
 #         is used.
-install_bash_completion() {
-    GH_RELEASE_BASH__COMP_FILE="$(basename "$1")"
+install_bash_completion() (
+    COMPLETION_FILE="$(basename "$1")"
 
-    log_info "Install bash completion for $GH_RELEASE_BASH__COMP_FILE"
+    log_info "Install bash completion for $COMPLETION_FILE"
     mkdir --parents "$BASH_COMPLETIONS_DIR"
-    GH_RELEASE_BASH__OUT="$BASH_COMPLETIONS_DIR/$GH_RELEASE_BASH__COMP_FILE"
-    log_debug "Write bash completion at $GH_RELEASE_BASH__OUT"
-    cat > "$GH_RELEASE_BASH__OUT"
-
-    unset GH_RELEASE_BASH__COMP_FILE GH_RELEASE_BASH__OUT
-}
+    COMPLETION_PATH="$BASH_COMPLETIONS_DIR/$COMPLETION_FILE"
+    log_debug "Write bash completion at $COMPLETION_PATH"
+    cat > "$COMPLETION_PATH"
+)
 
 # This function installs a zsh completion file received via STDIN.
 #
@@ -166,13 +152,11 @@ install_bash_completion() {
 #   - $1: The name for the completion file. If it's a path, only the base name
 #         is used. An '_' prefix is added if not already present.
 install_zsh_completion() {
-    GH_RELEASE_ZSH__COMP_FILE="$(basename "$1")"
+    COMPLETION_FILE="$(basename "$1")"
 
-    log_info "Install zsh completion for $GH_RELEASE_ZSH__COMP_FILE"
+    log_info "Install zsh completion for $COMPLETION_FILE"
     mkdir --parents "$ZSH_COMPLETIONS_DIR"
-    GH_RELEASE_ZSH__OUT="$ZSH_COMPLETIONS_DIR/_${GH_RELEASE_ZSH__COMP_FILE##_}"
-    log_debug "Write zsh completion at $GH_RELEASE_ZSH__OUT"
-    cat > "$GH_RELEASE_ZSH__OUT"
-
-    unset GH_RELEASE_ZSH__COMP_FILE GH_RELEASE_ZSH__OUT
+    COMPLETION_PATH="$ZSH_COMPLETIONS_DIR/_${COMPLETION_FILE##_}"
+    log_debug "Write zsh completion at $COMPLETION_PATH"
+    cat > "$COMPLETION_PATH"
 }

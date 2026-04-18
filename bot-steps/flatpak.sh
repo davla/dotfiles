4ab4921449df977d9a7431295cfbcbe86f445f37
Davide Laezza <truzzialrogo@gmx.com>
2026-04-18 20:26:38 +0200

    feature(flatpak): add File Roller

--- bot-steps/flatpak.sh
+++ bot-steps/flatpak.sh
@@ -39,4 +39,5 @@ esac
 print_info 'Install Flatpak applications shared across all machines'
 # No Bitwarden as it still has copy-paste issues on Wayland
 flatpak install --noninteractive adwaita-dark baobab org.gnome.clocks drawing \
-    org.geany.Geany io.mpv.Mpv pwvucontrol seahorse simplescan spotify
+    fileroller org.geany.Geany io.mpv.Mpv pwvucontrol seahorse simplescan \
+    spotify

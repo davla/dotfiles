fef1fdaf2e50f2c0fc43f21ac58c662545ca1ea1
Davide Laezza <truzzialrogo@gmx.com>
2026-09-10 17:19:31 +0200

    feature: add lazysql package

--- bot-steps/arch/packages.sh
+++ bot-steps/arch/packages.sh
@@ -84,9 +84,9 @@ fi
 print_info 'Install CLI packages shared across all machines'
 sudo --user "$USER_NAME" yay -S --needed autoconf automake bat bind cmake \
     cmatrix cowsay curl debugedit devbox-bin dkms dos2unix eza fasd \
-    fortune-mod fzf gcc git-secret gnupg htop jq lua luacheck luarocks man \
-    mercurial mmv moreutils multi-git-status myrepos nfs-utils nyancat \
-    otf-ipafont pacman-contrib passt pkgfile playerctl podman-compose \
+    fortune-mod fzf gcc git-secret gnupg htop jq lazysql-bin lua luacheck \
+    luarocks man mercurial mmv moreutils multi-git-status myrepos nfs-utils \
+    nyancat otf-ipafont pacman-contrib passt pkgfile playerctl podman-compose \
     podman-docker python python-pip rbw sheldon sl sudo thefuck ttf-baekmuk \
     ttf-dejavu ttf-indic-otf ttf-khmer ttf-nerd-fonts-symbols \
     ttf-nerd-fonts-symbols-mono unzip uv vim wqy-microhei-lite zip
